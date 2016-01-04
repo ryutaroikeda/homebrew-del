87f1342e4aaf8ec72b555fcb49978b6f0209582a
Ryutaro Ikeda <ryutaroikeda94@gmail.com>
2016-01-04 17:43:38 +0000

    added sha256 key and repackaged tar

--- Formula/del.rb
+++ Formula/del.rb
@@ -3,6 +3,7 @@ class Del < Formula
   homepage "https://github.com/ryutaroikeda/del"
   url "https://raw.github.com/ryutaroikeda/homebrew-del/master/"\
 "del20160104.tar.gz"
+  sha256 "31cc36f481b9f676e6fe8db32d390b577e54ab58ce0e5e810e0112fc962d1aa0"
   def install
     system "make"
     system "make", "install"
