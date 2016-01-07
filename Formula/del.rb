e267d9b3f99de33bdd12f49927ae5e7bd9ad39f2
Ryutaro Ikeda <ryutaroikeda94@gmail.com>
2016-01-07 19:44:13 +0000

    New tarball

--- Formula/del.rb
+++ Formula/del.rb
@@ -3,10 +3,12 @@ class Del < Formula
   homepage "https://github.com/ryutaroikeda/del"
   url "https://raw.github.com/ryutaroikeda/homebrew-del/master/"\
 "del20160104.tar.gz"
-  sha256 "31cc36f481b9f676e6fe8db32d390b577e54ab58ce0e5e810e0112fc962d1aa0"
+  sha256 "4c672cbdddc9f072d6d4265bdbd3ea522b5c186ffe3182c783c6bb27f64c5228"
+  version "0.1"
+
   def install
     system "make"
-    system "make", "install"
+    bin.install "bin/del"
   end
 
   test do
