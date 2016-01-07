ddffe6f0d146650a62ae20a34058c57f35532cff
Ryutaro Ikeda <ryutaroikeda94@gmail.com>
2016-01-07 19:56:35 +0000

    Updated formula

--- Formula/del.rb
+++ Formula/del.rb
@@ -2,7 +2,7 @@ class Del < Formula
   desc "Trash utility"
   homepage "https://github.com/ryutaroikeda/del"
   url "https://raw.github.com/ryutaroikeda/homebrew-del/master/"\
-"del20160104.tar.gz"
+"del20160107.tar.gz"
   sha256 "4c672cbdddc9f072d6d4265bdbd3ea522b5c186ffe3182c783c6bb27f64c5228"
   version "0.1"
 
